ea4f4b2b590c30b980070338b8173ea1367ac84c
peterhabib <38513546+peterhabib@users.noreply.github.com>
2019-04-11 18:16:25 +0200

    Update settings.py

--- COPR_Project/settings.py
+++ COPR_Project/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = "/Core/static/"
 
 django_heroku.settings(locals())
